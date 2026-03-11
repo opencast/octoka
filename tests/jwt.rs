ed82047642825f39d759203e3e0e953cbd81e8e4
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2026-03-11 14:28:00 +0100

    Add keys and tests for ES256, RS256 and PS256

--- tests/jwt.rs
+++ tests/jwt.rs
@@ -99,10 +99,18 @@ macro_rules! assert_jwt_ok_but_forbidden {
 /// Encoded header: `{ "alg": "EdDSA", "typ": "JWT" }`
 const HEADER_EDDSA: &str = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9";
 
+/// Encoded header: `{ "alg": "ES256", "typ": "JWT" }`
+const HEADER_ES256: &str = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9";
+
+/// Encoded header: `{ "alg": "RS256", "typ": "JWT" }`
+const HEADER_RS256: &str = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9";
+
+/// Encoded header: `{ "alg": "PS256", "typ": "JWT" }`
+const HEADER_PS256: &str = "eyJhbGciOiJQUzI1NiIsInR5cCI6IkpXVCJ9";
+
 /// Encoded payload: `{ "exp": 4012345678, "roles": ["ROLE_ADMIN"] }` (exp is Feb 2097)
 const PAYLOAD_ADMIN: &str = "eyJleHAiOjQwMTIzNDU2NzgsInJvbGVzIjpbIlJPTEVfQURNSU4iXX0";
 
-/// With only `ed25519` key.
 #[tokio::test]
 async fn ed25519() -> Result<()> {
     let setup = setup(&["ed25519.json"]).await?;
@@ -122,7 +130,107 @@ async fn ed25519() -> Result<()> {
     let jwt = format!("{HEADER_EDDSA}.{PAYLOAD_ADMIN}.\
         yftxGDuDRHgWz8WUifYRmwtI9RkMkWv6OmS0Pezx39MHA2w-lAsCdGv01fIK1xBvp1F1BO5WnyZsJZmQaovQCA");
     assert_status!(setup.fetch(&jwt), StatusCode::FORBIDDEN);
+    let jwt = format!("{HEADER_EDDSA}.{PAYLOAD_ADMIN}.");
+    assert_status!(setup.fetch(&jwt), StatusCode::FORBIDDEN);
 
+    Ok(())
+}
+
+#[tokio::test]
+async fn es256() -> Result<()> {
+    let setup = setup(&["es256.json"]).await?;
+
+    // Valid signature
+    let jwt = format!("{HEADER_ES256}.{PAYLOAD_ADMIN}.\
+        t12mLMa67e_XagL0SnLC87sT853ksnQ1UkWIaIlZCl1gYlvyDvvH5UL1IA1TZ2S0XzISUGMeCIqAexhx0-gm5w");
+    assert_status!(setup.fetch(&jwt), StatusCode::NO_CONTENT);
+
+    // Invalid signatures
+    let jwt = format!("{HEADER_ES256}.{PAYLOAD_ADMIN}.\
+        t12mLMa67e_XagL0SnLC87sT853ksnQ1UkWIaIlZCl1gYlvyDvvH5UL1IA1TZ2S0XzISUGMeCIqAexhx0-gm5");
+    assert_status!(setup.fetch(&jwt), StatusCode::FORBIDDEN);
+    let jwt = format!("{HEADER_ES256}.{PAYLOAD_ADMIN}.\
+        12mLMa67e_XagL0SnLC87sT853ksnQ1UkWIaIlZCl1gYlvyDvvH5UL1IA1TZ2S0XzISUGMeCIqAexhx0-gm5w");
+    assert_status!(setup.fetch(&jwt), StatusCode::FORBIDDEN);
+    let jwt = format!("{HEADER_ES256}.{PAYLOAD_ADMIN}.\
+        aFB6jlBnA-9F3vdwxSjz_LM3-LDNKvLsgptDHRb3G_d8Ny5hbbuUrjupDrKkgGpJl5cl5ySXonmO7qQ-qaCXww");
+    assert_status!(setup.fetch(&jwt), StatusCode::FORBIDDEN);
+    let jwt = format!("{HEADER_ES256}.{PAYLOAD_ADMIN}.");
+    assert_status!(setup.fetch(&jwt), StatusCode::FORBIDDEN);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn rs256() -> Result<()> {
+    let setup = setup(&["rs256.json"]).await?;
+
+    // Valid signature
+    let jwt = format!("{HEADER_RS256}.{PAYLOAD_ADMIN}.\
+        gXBdYDY_xFB4KpSkrTxz21y38-_w5x4xZM1qICK30V4cpyDXS3NW1wJCDWnJRz8F1MpMrGZOqCXIzM5rmGHuO-K4Q3T\
+        TT8vMY7onLAUpIaJFynuhNCgRlcAEoV_UJoFz5umAnVugtvn5MH1TsIaIP27skw347kW5UCIjUvJchViDG3pvHeIn42\
+        3tgelsH29vNjeqfCMPGAzukUMFAD4vKeTs_X1vpy9yB7DUOaY4kVBYcLaUGz81GlKwUYU21fZGvzvh7l6mhRfmWiFSe\
+        jKQRfrxWokH1hID1am-7D_LUX5vw5Pbs0HbZyFJd7xYCXWFMFzpLk0SUF1SgwOsfwmvjA");
+    assert_status!(setup.fetch(&jwt), StatusCode::NO_CONTENT);
+
+    // Invalid signatures
+    let jwt = format!("{HEADER_RS256}.{PAYLOAD_ADMIN}.\
+        gXBdYDY_xFB4KpSkrTxz21y38-_w5x4xZM1qICK30V4cpyDXS3NW1wJCDWnJRz8F1MpMrGZOqCXIzM5rmGHuO-K4Q3T\
+        TT8vMY7onLAUpIaJFynuhNCgRlcAEoV_UJoFz5umAnVugtvn5MH1TsIaIP27skw347kW5UCIjUvJchViDG3pvHeIn42\
+        3tgelsH29vNjeqfCMPGAzukUMFAD4vKeTs_X1vpy9yB7DUOaY4kVBYcLaUGz81GlKwUYU21fZGvzvh7l6mhRfmWiFSe\
+        jKQRfrxWokH1hID1am-7D_LUX5vw5Pbs0HbZyFJd7xYCXWFMFzpLk0SUF1SgwOsfwmvj");
+    assert_status!(setup.fetch(&jwt), StatusCode::FORBIDDEN);
+    let jwt = format!("{HEADER_RS256}.{PAYLOAD_ADMIN}.\
+        XBdYDY_xFB4KpSkrTxz21y38-_w5x4xZM1qICK30V4cpyDXS3NW1wJCDWnJRz8F1MpMrGZOqCXIzM5rmGHuO-K4Q3TT\
+        T8vMY7onLAUpIaJFynuhNCgRlcAEoV_UJoFz5umAnVugtvn5MH1TsIaIP27skw347kW5UCIjUvJchViDG3pvHeIn423\
+        tgelsH29vNjeqfCMPGAzukUMFAD4vKeTs_X1vpy9yB7DUOaY4kVBYcLaUGz81GlKwUYU21fZGvzvh7l6mhRfmWiFSej\
+        KQRfrxWokH1hID1am-7D_LUX5vw5Pbs0HbZyFJd7xYCXWFMFzpLk0SUF1SgwOsfwmvjA");
+    assert_status!(setup.fetch(&jwt), StatusCode::FORBIDDEN);
+    let jwt = format!("{HEADER_RS256}.{PAYLOAD_ADMIN}.\
+        NhfxmwOFm_nOX3z2k8bO_z831FjEa7Ed2E84CGrlJJN2aWyKrEyQqepgRsx3bhHJT1y2d-4ETE2iAhkkVPcQ1Izw5kj\
+        tm8QY6jqbG27hG9XR3gUMEKIAondZDKQBC2P_3V3vU-xehkliGbGOQafVFCToSeamkOF1lyXzrRjo7TjkR7cxz3Mgih\
+        d78Ow7XLNYW3gWWgTi81qxJOWMQ1n5Azn_4AzWkGljKP612rlkyYNaaiJcz740M771VLsyEXQDf3oYWB3zta-YRSjrl\
+        q16AZGI0RUajsB3qITWYB3-gWVZf5oqCkHevA5mfRSUs9s7XHBV4prLLEzv1BO6LdoPhg");
+    assert_status!(setup.fetch(&jwt), StatusCode::FORBIDDEN);
+    let jwt = format!("{HEADER_RS256}.{PAYLOAD_ADMIN}.");
+    assert_status!(setup.fetch(&jwt), StatusCode::FORBIDDEN);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn ps256() -> Result<()> {
+    let setup = setup(&["ps256.json"]).await?;
+
+    // Valid signature
+    let jwt = format!("{HEADER_PS256}.{PAYLOAD_ADMIN}.\
+        fbrwUDO3A72-nri9Lwls_VgI3wAz2WZtUttPs39crkNo8-vnU8g-XnmTP5d6FbqqiVPydZhft7IQUzxk_FSRUkyGGOA\
+        qn18boquUMPs8dmpkwCCQP6s2rHcR0PuHTUaW39HnP72Uo4NfembVLF4YgrR0WK1vRqEwbOWi23jFUhveScmIbZXEzT\
+        WrUdIHe554cpoV9EJkozKcYQts4mtYZFCXpH_bMErUEXn4sRe7oR_cSxKGYKlPoDWV2ARiUjggVBJcKFwTKsy9UK16W\
+        jHzOEPespE6wz04fkx7JyCmCd3norg2X5Feb7IWy48A2n4NwZkmIuSfHO-FZwyXwxuOFw");
+    assert_status!(setup.fetch(&jwt), StatusCode::NO_CONTENT);
+
+    // Invalid signatures
+    let jwt = format!("{HEADER_PS256}.{PAYLOAD_ADMIN}.\
+        fbrwUDO3A72-nri9Lwls_VgI3wAz2WZtUttPs39crkNo8-vnU8g-XnmTP5d6FbqqiVPydZhft7IQUzxk_FSRUkyGGOA\
+        qn18boquUMPs8dmpkwCCQP6s2rHcR0PuHTUaW39HnP72Uo4NfembVLF4YgrR0WK1vRqEwbOWi23jFUhveScmIbZXEzT\
+        WrUdIHe554cpoV9EJkozKcYQts4mtYZFCXpH_bMErUEXn4sRe7oR_cSxKGYKlPoDWV2ARiUjggVBJcKFwTKsy9UK16W\
+        jHzOEPespE6wz04fkx7JyCmCd3norg2X5Feb7IWy48A2n4NwZkmIuSfHO-FZwyXwxuOF");
+    assert_status!(setup.fetch(&jwt), StatusCode::FORBIDDEN);
+    let jwt = format!("{HEADER_PS256}.{PAYLOAD_ADMIN}.\
+        brwUDO3A72-nri9Lwls_VgI3wAz2WZtUttPs39crkNo8-vnU8g-XnmTP5d6FbqqiVPydZhft7IQUzxk_FSRUkyGGOAq\
+        n18boquUMPs8dmpkwCCQP6s2rHcR0PuHTUaW39HnP72Uo4NfembVLF4YgrR0WK1vRqEwbOWi23jFUhveScmIbZXEzTW\
+        rUdIHe554cpoV9EJkozKcYQts4mtYZFCXpH_bMErUEXn4sRe7oR_cSxKGYKlPoDWV2ARiUjggVBJcKFwTKsy9UK16Wj\
+        HzOEPespE6wz04fkx7JyCmCd3norg2X5Feb7IWy48A2n4NwZkmIuSfHO-FZwyXwxuOFw");
+    assert_status!(setup.fetch(&jwt), StatusCode::FORBIDDEN);
+    let jwt = format!("{HEADER_PS256}.{PAYLOAD_ADMIN}.\
+        d3Hn4YqkL3ZR4BGbgHXMUux796GifChPpkg_zXoF7QJ4uQzrWJsVXYdSNSeV4bNG3OCaErPrqjM1I0WkXRVNi5iusj5\
+        5UpR6NQIyZ445IjAj6PxbpuN4uztTu9tMJ1-Q_VPHbl5PlGlxfuk4o3aBoBxcs-xniTQrocUehp7yvQ0hkKcg68wH78\
+        Mo40fORSXjhuTPz4l7t4W1H_S58jVB67E2dEF4fY8xtZnP6m8qzZgcGfjfNqW1LJ0FVEZ-J5SwzWa_SNSPETjGk4Np8\
+        vOYsCwuWGnypYqQf_u6MOcpJChI_H-ZAj2tiPFBkfZKPaaF6FGzzWjLfCBlfzBLPVMKOg");
+    assert_status!(setup.fetch(&jwt), StatusCode::FORBIDDEN);
+    let jwt = format!("{HEADER_PS256}.{PAYLOAD_ADMIN}.");
+    assert_status!(setup.fetch(&jwt), StatusCode::FORBIDDEN);
 
     Ok(())
 }
